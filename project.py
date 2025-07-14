58f602beffb694bf87a1013a21d9e4a5e2c5f35f
Kunalgit8 <kunalkundu2024@gmail.com>
2025-07-14 15:56:08 +0530

    Add files via upload

---
+++ project.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import matplotlib as plt
+import streamlit as st
+import os
+
+pressed = st.button("1 press = 1 smash")
+if pressed:
+    st.write("smashed reload to smash again")
+st.divider()
+st.image(os.path.join(os.getcwd(), "static", "lostless.png"))
